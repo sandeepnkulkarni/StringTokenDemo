58860e6b215dd08a39e468724c43140bd750d224
Sandeep Kulkarni <sandeep@snk.local>
2017-10-20 12:31:51 +0530

    Demo Maven Java project.
    It uses StrTokenizer from org.apache.commons.lang3.text to split input
    string based on delimiter and prints token list on console.

---
+++ src/local/snk/StringTokenDemo.java
@@ -0,0 +1,34 @@
+package local.snk;
+
+import java.util.List;
+import org.apache.commons.lang3.text.*;
+
+public class StringTokenDemo {
+
+	/**
+	 * This is main method.
+	 * It uses StrTokenizer from org.apache.commons.lang3.text to split input string based on delimiter
+	 * and prints token list on console.
+	 * @param args	Input argument for the program.
+	 */
+    public static void main(String[] args) {
+        String test = "pdf, doc";
+	    List<String> tokens = new StrTokenizer(test, ",").setTrimmerMatcher(StrMatcher.spaceMatcher()).getTokenList();
+	    for (String token : tokens)
+        {
+            System.out.printf("[%s]\n", token);
+        }
+	    
+	    TestFun();
+    }
+    
+    /**
+     * Test function which print 'Test Function' on console.
+     * @return	void
+     */
+    private static void TestFun()
+    {
+    	System.out.printf("Test Function\n");
+    }
+
+}
